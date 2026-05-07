29a50222f934dd6f19ff9ca1da1cf2a2ecc8b177
hina0629 <hina.0629.xxx@gmail.com>
2026-05-07 16:29:01 +0900

    ヘッダーとフッターコンポーネントをインポート

--- react-hello/src/App.jsx
+++ react-hello/src/App.jsx
@@ -1,13 +1,19 @@
 import styles from './App.module.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className={styles.app}>
+      <Header />
+
       <main className={styles.main}>
         <h2 className={styles.sectionTitle}>Anime Station</h2>
         <div>ここになんか作品</div>
       </main>
+
+      <Footer />
     </div>
   )
 }
