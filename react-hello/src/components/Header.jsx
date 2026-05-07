f128502cbdd8662d26f388f1615c0888f74ca13b
hina0629 <hina.0629.xxx@gmail.com>
2026-05-07 16:55:47 +0900

    ロゴのCSS

--- react-hello/src/components/Header.jsx
+++ react-hello/src/components/Header.jsx
@@ -1,10 +1,13 @@
 // rfce
 import styles from './Header.module.css'
+import logo from '../assets/logo.svg'
 
 function Header() {
   return (
     <header className={styles.header}>
-      
+      <div className={styles.logo}>
+        <img src={logo} alt="Logo" />
+      </div>
       <div className={styles.title}>Anime Station</div>
     </header>
   )
