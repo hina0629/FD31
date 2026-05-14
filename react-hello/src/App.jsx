2a47f35020d93e3302fc63b921a48d1744c69aed
hina0629 <hina.0629.xxx@gmail.com>
2026-05-14 15:42:08 +0900

    不要な行をコメントアウト

--- react-hello/src/App.jsx
+++ react-hello/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
       <main className={styles.main}>
         <h2 className={styles.sectionTitle}>Anime Station</h2>
-        <div>ここになんか作品</div>
+        {/* <div>ここになんか作品</div> */}
         <WorkList works={works} />
         {/* {...works} でも可能 */}
       </main>
