76cf121feb0be4e3b60974639a12ca71f628e157
hina0629 <hina.0629.xxx@gmail.com>
2026-05-14 17:09:06 +0900

    WorkCardコンポーネントにスタイルを追加

--- react-hello/src/components/WorkCard.jsx
+++ react-hello/src/components/WorkCard.jsx
@@ -1,9 +1,25 @@
+import styles from './WorkCard.module.css'
 
 function WorkCard({ work, onSelect }) {
   return (
-    <div key={work.id} onClick={() => onSelect(work)}>
+    <div className={styles.card} key={work.id} onClick={() => onSelect(work)}>
       {/* keyを入れないと、表示はされるけどエラーが出る */}
-      {work.title}
+      {/* 画像 */}
+      <div className={styles.thumbnailWrapper}>
+        <img className={styles.thumbnail} src={work.image} alt={work.title} />
+      </div>
+
+      {/* 作品の情報 */}
+      <div className={styles.body}>
+        {/* 作品のメタ情報 */}
+        <div className={styles.meta}>
+          <span className={styles.genre}>{work.genre}</span>
+          <span className={styles.year}>{work.year}</span>
+        </div>
+
+        <h2 className={styles.title}>{work.title}</h2>
+        <p className={styles.description}>{work.description}</p>
+      </div>
     </div>
   )
 }
