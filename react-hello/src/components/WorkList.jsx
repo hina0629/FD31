6e066b0f45c01e7744037204d3a7f115efc9bf29
hina0629 <hina.0629.xxx@gmail.com>
2026-05-14 15:59:43 +0900

    WorkListコンポーネントにonSelectプロパティを追加し、作品をクリック可能に

--- react-hello/src/components/WorkList.jsx
+++ react-hello/src/components/WorkList.jsx
@@ -1,12 +1,12 @@
 
-function WorkList({works}) {
+function WorkList({ works, onSelect }) {
   return (
     <div>
       <div>{works.length}作品</div>
       <div>
         {
           works.map(work => (
-            <div key={work.id}>
+            <div key={work.id} onClick={() => onSelect(work)}>
               {/* keyを入れないと、表示はされるけどエラーが出る */}
               {work.title}
             </div>
