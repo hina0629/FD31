788ae547a4a6e4366ea0e8af8aa2a5af48dd90b2
hina0629 <hina.0629.xxx@gmail.com>
2026-06-18 15:53:48 +0900

    Layoutコンポーネントのスタイルを適用

--- react-profile/src/components/Layout.jsx
+++ react-profile/src/components/Layout.jsx
@@ -1,21 +1,23 @@
+import styles from "./Layout.module.css";
+
 function Layout({ children, onNavigate }) {
   return (
-    <div>
-      <header>
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
         <h1>サイトタイトル</h1>
 
-        <nav>
+        <nav className={styles.nav}>
           {/* aタグよりbuttonタグ推奨 */}
-          <button onClick={() => onNavigate('home')}>Home</button>
-          <button onClick={() => onNavigate('about')}>About</button>
+          <button onClick={() => onNavigate('home')} className={styles.navButton}>Home</button>
+          <button onClick={() => onNavigate('about')} className={styles.navButton}>About</button>
         </nav>
       </header>
 
-      <main>
+      <main className={styles.main}>
         { children }
       </main>
 
-      <footer>
+      <footer className={styles.footer}>
         <p>&copy; 2026 My App</p>
       </footer>
     </div>
