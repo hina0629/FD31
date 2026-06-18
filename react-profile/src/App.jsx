460fd37011031092dd76366a6f169d978f13a5f3
hina0629 <hina.0629.xxx@gmail.com>
2026-06-18 15:33:31 +0900

    Appコンポーネントにページ切替機能を追加

--- react-profile/src/App.jsx
+++ react-profile/src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About"
 
 function App() {
+  const [page, setPage] = useState('home')
+
   return (
     <Layout>
-      メインページ
+      {page === 'home' &&  <Home />}
+      {page === 'about' && <About />}
     </Layout>
   )
 }
