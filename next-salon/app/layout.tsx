2b75505a69eac8014140efa85ecbf60309fcccfa
hina0629 <hina.0629.xxx@gmail.com>
2026-07-09 15:31:55 +0900

    feat: コンテンツをメインタグでラップ

--- next-salon/app/layout.tsx
+++ next-salon/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
     <html lang="ja">
       <body className="min-h-full flex flex-col">
         <Header />
-        {children}
+        <main className="mx-auto max-w-7xl px-8 py-4">
+          {children}
+        </main>
       </body>
     </html>
   );
