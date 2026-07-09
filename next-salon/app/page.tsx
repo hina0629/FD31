48946c0bb680b294d6370e44cb67dfc7e9948bec
hina0629 <hina.0629.xxx@gmail.com>
2026-07-09 16:00:26 +0900

    feat: SectionHeadingコンポーネントを使用

--- next-salon/app/page.tsx
+++ next-salon/app/page.tsx
@@ -1,11 +1,15 @@
 // サーバーコンポーネント
 
+import SectionHeading from "@/components/SectionHeading";
+
 export default function Home() {
   return (
   <div>
-    <h1 className="font-bold text-3xl text-center py-4">
-      Welcom Next Salon
-    </h1>
+    <SectionHeading 
+    eyebrow="Featured Shops"
+    title="今日の気分から選べるサロン"
+    description="エリアやメニューから、あなたにぴったりのサロンを見つけましょう。"
+    />
   </div>
   );
 }
