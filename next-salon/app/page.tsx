49b556f2a475f1d122ee4c96c631cbe5e0326c2b
hina0629 <hina.0629.xxx@gmail.com>
2026-09-03 23:06:00 +0900

    feat: ShopCardコンポーネントのグリッドにgapを追加

--- next-salon/app/page.tsx
+++ next-salon/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       {/* md:grid-cols-2 lgよりも早い段階で2列にする */}
       {/* レイアウトはCSSフレームワークを使うこともある */}
       {/* mt-8 margin-topを広げる */}
-      <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3">
+      <div className="mt-8 gap-2 grid md:grid-cols-2 lg:grid-cols-3">
         {shops.map((shop) => (
           // 繰り返すときは key を書く
           <ShopCard key={shop.id} shop={shop} />
