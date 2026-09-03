11c9a245f2374f1fe7afb3d95674ac383b1f4104
hina0629 <hina.0629.xxx@gmail.com>
2026-09-03 17:12:30 +0900

    feat: ShopCardコンポーネントのスタイルを改善し、画像と詳細情報を追加 feat: Homeコンポーネントにグリッドレイアウトを実装 feat: 新しい画像ファイルを追加

--- next-salon/app/page.tsx
+++ next-salon/app/page.tsx
@@ -16,7 +16,13 @@ export default function Home() {
         title="今日の気分から選べるサロン"
         description="エリアやメニューから、あなたにぴったりのサロンを見つけましょう。"
       />
-      <div>
+      {/* grid 今からグリッドするっていう宣言 */}
+      {/* grid-cols-3 3列にする */}
+      {/* lg:grid-cols-3 ある一定の領域から縦になる */}
+      {/* md:grid-cols-2 lgよりも早い段階で2列にする */}
+      {/* レイアウトはCSSフレームワークを使うこともある */}
+      {/* mt-8 margin-topを広げる */}
+      <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3">
         {shops.map((shop) => (
           // 繰り返すときは key を書く
           <ShopCard key={shop.id} shop={shop} />
