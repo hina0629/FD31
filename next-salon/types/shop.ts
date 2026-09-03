6f67fc2ef0e7663c19c789eba9e9b64a7aae0716
hina0629 <hina.0629.xxx@gmail.com>
2026-09-03 15:14:32 +0900

    feat: モックデータ投入

---
+++ next-salon/types/shop.ts
@@ -0,0 +1,11 @@
+export type Shop = {
+  id: number
+  name: string
+  area: string
+  rating: number
+  reviewCount: number
+  tags: string[]
+  description: string
+  coverImage: string
+  leadMessage: string
+}
