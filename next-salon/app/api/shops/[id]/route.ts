9025a69a8df0906f898682d16c694f9791a29c5d
hina0629 <hina.0629.xxx@gmail.com>
2026-09-17 15:13:01 +0900

    api/[id] でショップの情報が取れる機能の追加

---
+++ next-salon/app/api/shops/[id]/route.ts
@@ -0,0 +1,18 @@
+import { MOCK_SHOPS } from "@/data/MockData";
+import { NextResponse } from "next/server";
+
+// データを受け取る型を定義
+type Params = { params: Promise<{ id: string}> }
+
+export async function GET({ params }: Params) {
+    // Promise は非同期なので await が使える
+    // const { id }　は　Promise<{ id: string}> のデータ型と一致している
+    const { id } = await params
+    // shop を検索
+    // 配列のデータの検索方法
+    const shop = MOCK_SHOPS.find((s) => s.id === id)
+
+    // JSONでレスポンス
+    // return でデータを渡す
+    return NextResponse.json({ shop })
+}
