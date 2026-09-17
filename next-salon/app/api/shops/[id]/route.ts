027d21ee618810865f13432bebfbd181eadc5754
hina0629 <hina.0629.xxx@gmail.com>
2026-09-17 15:20:42 +0900

    Requestについての補足コメント

--- next-salon/app/api/shops/[id]/route.ts
+++ next-salon/app/api/shops/[id]/route.ts
@@ -4,6 +4,11 @@ import { NextResponse } from "next/server";
 // データを受け取る型を定義
 type Params = { params: Promise<{ id: string}> }
 
+// Router Handler(route.ts) にアクセス（HTTPリクエスト）があったすべての時に、
+// Next.js サーバーが自動的に第1引数へリクエストオブジェクト（Request）を渡すため、
+// _request がないと Params に入ってしまいエラーになる
+// アンダースコアをつけているのは、TSに対してこの引数は位置合わせのために用意しているが、
+// 関数内で使用しない変数であることを伝えるための慣習的な記述
 export async function GET(_request: Request, { params }: Params) {
     // Promise は非同期なので await が使える
     // const { id }　は　Promise<{ id: string}> のデータ型と一致している
