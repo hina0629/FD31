43fa5e5431cc5bfd06c11025b7ee20aca0c4e5f5
hina0629 <hina.0629.xxx@gmail.com>
2026-09-24 17:01:35 +0900

    feat: BookingPageコンポーネントにprops型定義を追加し、コメントを修正

--- next-salon/app/shop/[id]/book/page.tsx
+++ next-salon/app/shop/[id]/book/page.tsx
@@ -1,9 +1,13 @@
 // "use cliant"
+// useState は use cliant を書かないと使えない
 import SectionHeading from '@/components/SectionHeading'
 import React from 'react'
 
-// useState は use cliant を書かないと使えない
-export default function BookingPage() {
+type BookingPageProps = {
+  params: Promise<{ id: string }>;
+}
+
+export default async function BookingPage({params}: BookingPageProps) {
   return (
     <div>
       <SectionHeading
