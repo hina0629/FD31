e26f9664254778d382989463bfdfe78acc290821
hina0629 <hina.0629.xxx@gmail.com>
2026-09-24 17:04:27 +0900

    feat: 予約ページでショップ名を動的に表示するように変更

--- next-salon/app/shop/[id]/book/page.tsx
+++ next-salon/app/shop/[id]/book/page.tsx
@@ -1,6 +1,8 @@
 // "use cliant"
 // useState は use cliant を書かないと使えない
 import SectionHeading from '@/components/SectionHeading'
+import { getShop } from '@/lib/services/shops';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 type BookingPageProps = {
@@ -8,11 +10,16 @@ type BookingPageProps = {
 }
 
 export default async function BookingPage({params}: BookingPageProps) {
+  const { id } = await params;
+  const shop = await getShop(id)
+
+  if (!shop) notFound()
+
   return (
     <div>
       <SectionHeading
         eyebrow="Booking"
-        title="予約"
+        title={`${shop.name} の予約`}
         description="メニュー・スタッフ・日時を選んで予約します。"
       />
     </div>
