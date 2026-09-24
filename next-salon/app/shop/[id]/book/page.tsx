84390b866a9cbf4d82869d74387513fa25dd1c4a
hina0629 <hina.0629.xxx@gmail.com>
2026-09-24 16:51:25 +0900

    feat: 予約ページコンポーネントの作成

---
+++ next-salon/app/shop/[id]/book/page.tsx
@@ -0,0 +1,14 @@
+import SectionHeading from '@/components/SectionHeading'
+import React from 'react'
+
+export default function BookingPage() {
+  return (
+    <div>
+      <SectionHeading
+        eyebrow="Booking"
+        title="予約"
+        description="メニュー・スタッフ・日時を選んで予約します。"
+      />
+    </div>
+  )
+}
